b8fa8be10119d4365e769e159b97140b01f3c598
David James <davidcjames+xpe@gmail.com>
2016-05-25 13:23:55 -0700

    WIP: builds and runs

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,9 @@ struct Board([SBoard; 9]);
 // representation requires 16 bits.
 bitflags! {
     flags SBoard: u16 {
-        const ROW_0 = 0b0000000000011111,
-        const ROW_1 = 0b0000001111100000,
-        const ROW_2 = 0b0111110000000000,
+        const R0 = 0b0000000000011111,
+        const R1 = 0b0000001111100000,
+        const R2 = 0b0111110000000000,
     }
 }
 
@@ -34,10 +34,26 @@ enum Slot { Taken(Player), Empty }
 // A Move has a row and a column
 bitflags! {
     flags Move: u8 {
-        const ROW = 0b00001111,
-        const COL = 0b11110000,
+        const R = 0b00001111,
+        const C = 0b11110000,
     }
 }
+
+// A sub-board move
+#[derive(Debug)]
+struct SMove { coord: SCoord, player: Player }
+
+// A sub-board coordinate
+#[derive(Debug)]
+struct SCoord { row: SRow, col: SCol }
+
+// A sub-board row: 0, 1, or 2
+#[derive(Debug)]
+enum SRow { R0, R1, R2 }
+
+// A sub-board column: 0, 1, or 2
+#[derive(Debug)]
+enum SCol { C0, C1, C2 }
 
 // ---------- functions ----------
 
@@ -49,18 +65,50 @@ fn empty_board() -> Board {
     Board([SBoard::empty(); 9])
 }
 
+// TODO: remove #[allow(unused_variables)]
 #[allow(unused_variables)]
 fn slot(sb: SBoard) -> Slot {
     Slot::Empty
 }
 
+// Plays (makes a move) on the sub-board and returns an updated sub-board.
+// TODO: remove #[allow(unused_variables)]
+#[allow(unused_variables)]
+fn play(sb: SBoard, sm: SMove) -> SCoord {
+    match sm.coord {
+        SCoord { row: r, col: c } => SCoord { row: r, col: c }
+    }
+}
+
+// ---------- debugging / testing ----------
+
 fn print_examples() {
+    println!("sub-board row 0 {:?}", SRow::R0);
+    println!("sub-board row 1 {:?}", SRow::R1);
+    println!("sub-board row 1 {:?}", SRow::R2);
+    println!("sub-board col 0 {:?}", SCol::C0);
+    println!("sub-board col 1 {:?}", SCol::C1);
+    println!("sub-board col 1 {:?}", SCol::C2);
+
+    println!("sub-board move {:?}", SMove {
+        coord: SCoord{row: SRow::R0, col: SCol::C2},
+        player: Player::X
+    });
+
     println!("player X : {:?}", Player::X);
     println!("player O : {:?}", Player::O);
+
     println!("slot - empty : {:?}", Slot::Empty);
     println!("slot - taken by X : {:?}", Slot::Taken(Player::X));
+
     println!("initial game : {:?}", init_game());
+
     println!("slot(empty SBoard) : {:?}", slot(SBoard::empty()));
+
+    println!("play() : {:?}",
+             play(SBoard::empty(),
+                  SMove{ coord: SCoord { row: SRow::R1, col: SCol::C1 },
+                         player: Player::X}));
 }
 
 fn main() {
