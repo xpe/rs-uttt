881e92ece311254fa206bd80c934c1951e11885a
David James <davidcjames+xpe@gmail.com>
2016-05-31 22:48:01 -0700

    refactor, improve random.rs

--- src/random.rs
+++ src/random.rs
@@ -1,15 +1,16 @@
 /// Random functions.
 
-use constants::{EMPTY_GAME};
-use data::{Game, Play, Loc, RI, CI};
-use rand::{Rng, ThreadRng};
-use rand;
+use constants::*;
+use data::*;
+use rand::{Rand, Rng, ThreadRng, thread_rng};
 use std::collections::LinkedList;
+
+// -- games --------------------------------------------------------------------
 
 /// Plays a game randomly from start to finish. Returns a linked list of
 /// games (where each is a 'step').
 pub fn random_game() -> LinkedList<Game> {
-    let rng: &mut ThreadRng = &mut rand::thread_rng();
+    let rng: &mut ThreadRng = &mut thread_rng();
     let mut games: LinkedList<Game> = LinkedList::new();
     let mut game: Game = EMPTY_GAME;
     loop {
@@ -18,7 +19,7 @@ pub fn random_game() -> LinkedList<Game> {
             println!("\nGame is over.");
             break;
         } else {
-            game = game.play(random_play(game, rng)).unwrap();
+            game = game.play(random_valid_play(game, rng)).unwrap();
         }
         let x = rng.gen::<u8>();
         if x < 10 {
@@ -28,53 +29,124 @@ pub fn random_game() -> LinkedList<Game> {
     games
 }
 
-pub fn random_play(game: Game, rng: &mut ThreadRng) -> Play {
+// -- game ---------------------------------------------------------------------
+
+// -- board --------------------------------------------------------------------
+
+// -- rows ---------------------------------------------------------------------
+
+// -- row ----------------------------------------------------------------------
+
+impl Rand for Row {
+    /// Returns a random board column index.
+    fn rand<R: Rng>(rng: &mut R) -> Self {
+        const CHOICES: [Row; 27] = [
+            Row::EEE, Row::EEO, Row::EEX,
+            Row::EOE, Row::EOO, Row::EOX,
+            Row::EXE, Row::EXO, Row::EXX,
+            Row::OEE, Row::OEO, Row::OEX,
+            Row::OOE, Row::OOO, Row::OOX,
+            Row::OXE, Row::OXO, Row::OXX,
+            Row::XEE, Row::XEO, Row::XEX,
+            Row::XOE, Row::XOO, Row::XOX,
+            Row::XXE, Row::XXO, Row::XXX,
+        ];
+        rng.choose(&CHOICES).unwrap().clone()
+    }
+}
+
+// -- board play ---------------------------------------------------------------
+
+/// Returns a random play for a given game.
+pub fn random_valid_play(game: Game, rng: &mut ThreadRng) -> Play {
     Play {
-        loc: random_loc(game, rng),
+        loc: random_valid_loc(game, rng),
         player: game.next_player().unwrap(),
     }
 }
 
-// TODO: implement this correctly
+// -- sub-board play -----------------------------------------------------------
+
+// -- board location -----------------------------------------------------------
+
+/// Returns a random valid location for a play in a game.
 #[allow(unused_variables)]
-pub fn random_loc(game: Game, rng: &mut ThreadRng) -> Loc {
+pub fn random_valid_loc(game: Game, rng: &mut ThreadRng) -> Loc {
+    // TODO: implement this correctly
     let ri = rng.gen::<RI>();
     let ci = rng.gen::<CI>();
     Loc::new(ri, ci)
 }
 
-impl rand::Rand for RI {
-    fn rand<R: Rng>(rng: &mut R) -> RI {
-        let x = rng.gen::<u16>();
-        match x % 9 {
-            0 => RI::R0,
-            1 => RI::R1,
-            2 => RI::R2,
-            3 => RI::R3,
-            4 => RI::R4,
-            5 => RI::R5,
-            6 => RI::R6,
-            7 => RI::R7,
-            8 => RI::R8,
-            _ => panic!("internal error"),
-        }
+// -- sub-board location -------------------------------------------------------
+
+// -- slots --------------------------------------------------------------------
+
+// -- slot --------------------------------------------------------------------
+
+// -- board indexes ------------------------------------------------------------
+
+impl Rand for RI {
+    /// Returns a random board row index.
+    fn rand<R: Rng>(rng: &mut R) -> Self {
+        const CHOICES: [RI; 9] = [
+            RI::R0,
+            RI::R1,
+            RI::R2,
+            RI::R3,
+            RI::R4,
+            RI::R5,
+            RI::R6,
+            RI::R7,
+            RI::R8,
+        ];
+        rng.choose(&CHOICES).unwrap().clone()
     }
 }
 
-impl rand::Rand for CI {
-    fn rand<R: Rng>(rng: &mut R) -> CI {
-        let x = rng.gen::<u16>();
-        match x % 9 {
-            0 => CI::C0,
-            1 => CI::C1,
-            2 => CI::C2,
-            3 => CI::C3,
-            4 => CI::C4,
-            5 => CI::C5,
-            6 => CI::C6,
-            7 => CI::C7,
-            8 => CI::C8,
-            _ => panic!("internal error"),
-        }
+impl Rand for CI {
+    /// Returns a random board column index.
+    fn rand<R: Rng>(rng: &mut R) -> Self {
+        const CHOICES: [CI; 9] = [
+            CI::C0,
+            CI::C1,
+            CI::C2,
+            CI::C3,
+            CI::C4,
+            CI::C5,
+            CI::C6,
+            CI::C7,
+            CI::C8,
+        ];
+        rng.choose(&CHOICES).unwrap().clone()
+    }
+}
+
+// -- sub-board indexes --------------------------------------------------------
+
+
+impl Rand for SRI {
+    /// Returns a random sub-board row index.
+    fn rand<R: Rng>(rng: &mut R) -> Self {
+        const CHOICES: [SRI; 3] = [SRI::R0, SRI::R1, SRI::R2];
+        rng.choose(&CHOICES).unwrap().clone()
+    }
+}
+
+impl Rand for SCI {
+    /// Returns a random sub-board column index.
+    fn rand<R: Rng>(rng: &mut R) -> Self {
+        const CHOICES: [SCI; 3] = [SCI::C0, SCI::C1, SCI::C2];
+        rng.choose(&CHOICES).unwrap().clone()
+    }
+}
+
+// -- player -------------------------------------------------------------------
+
+impl Rand for Player {
+    /// Returns a random board column index.
+    fn rand<R: Rng>(rng: &mut R) -> Self {
+        const CHOICES: [Player; 2] = [Player::X, Player::O];
+        rng.choose(&CHOICES).unwrap().clone()
     }
 }
