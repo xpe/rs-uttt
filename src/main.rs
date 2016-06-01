ed0b4404c3a1b2d9813cda43bd73cd94f47fc9bd
David James <davidcjames+xpe@gmail.com>
2016-06-01 02:01:38 -0700

    WIP: debugging main output

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ fn main() {
     for (i, game) in games.iter().enumerate() {
         println!("Move #{}\n", i);
         p(game);
-        println!("\n");
+        println!("\nGame::is_won -> {}\n", game.board.is_won());
+        // for sboard in game.board.sboards().iter() {
+        //     println!("SBoard::is_won -> {}", sboard.is_won());
+        // }
     }
 }
