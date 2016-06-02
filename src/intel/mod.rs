49aeda7d4585fa0614865460d20f4f87f11a2a8f
David James <davidcjames+xpe@gmail.com>
2016-06-02 12:04:43 -0700

    planning comments for Game::outcome_for()

--- src/intel/mod.rs
+++ src/intel/mod.rs
@@ -34,9 +34,51 @@ impl Game {
                 let outcomes = self.valid_plays().iter()
                     .map(|&play| self.play(play).unwrap().outcome_for(0))
                     .collect::<Vec<Option<Outcome>>>();
+                // Some possible `outcomes`:
+                //
+                // vec![]
+                // (no valid plays)
+                // Note: Perhaps this path should not be reached because depth=0
+                // should be checked first!
+                //
+                // vec![Some(WinX(0))]
+                // (one valid play; it wins the game for X)
+                //
+                // vec![Some(WinO(0))]
+                // (one valid play; it wins the game for O)
+                //
+                // vec![Some(Tie(0))]
+                // (one valid play; it ties the game)
+                //
+                // vec![None]
+                // (one valid play; outcome unknown)
+                //
+                // vec![Some(WinX(0)), None]
+                // (two valid plays: a win for X or an unknown outcome)
+                //
+                // vec![Some(WinO(0)), None]
+                // (two valid plays: a win for O or an unknown outcome)
+                //
+                // impossible: vec![Some(WinO(0)), Some(WinX(0))]
+                // (impossible; only X or O can win on the next play)
                 if outcomes.is_empty() {
                     None
                 } else {
+                    // These are the possible return values:
+                    //
+                    // If the next player is X:
+                    // Some(Outcome::WinX(0))
+                    // Some(Outcome::WinO(1))
+                    // Some(Outcome::Tie(0))
+                    // Some(Outcome::Tie(1))
+                    // None
+                    //
+                    // If the next player is O:
+                    // Some(Outcome::WinO(0))
+                    // Some(Outcome::WinX(1))
+                    // Some(Outcome::Tie(0))
+                    // Some(Outcome::Tie(1))
+                    // None
                     unimplemented!()
                 }
             },
