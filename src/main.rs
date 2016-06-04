43d7a43a7a6e94cec9c1feb84932ad9046131c7e
David James <davidcjames+xpe@gmail.com>
2016-06-03 17:44:29 -0700

    refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -97,22 +97,22 @@ fn run_solve_2<R: Rng>(rng: &mut R, trials: u16) {
             let games = random_games(rng);
             let mut games_iter = games.iter();
             let game_n = games_iter.next_back().unwrap();
-            games_iter.next_back().unwrap(); // game_n_1
-            let game_n_2 = games_iter.next_back().unwrap();
+            games_iter.next_back().unwrap(); // game_nm1
+            let game_nm2 = games_iter.next_back().unwrap();
 
             if VERBOSE { h(1, "Game N"); }
             if VERBOSE { pln(game_n); }
 
             if VERBOSE { h(1, "Game N-2"); }
-            if VERBOSE { pln(game_n_2); }
-            let sol_nm2_d1 = game_n_2.solve_for(1);
+            if VERBOSE { pln(game_nm2); }
+            let sol_nm2_d1 = game_nm2.solve_for(1);
             if VERBOSE { p_solution("N-2", 1, &sol_nm2_d1); }
-            let sol_nm2_d2 = game_n_2.solve_for(2);
+            let sol_nm2_d2 = game_nm2.solve_for(2);
             if VERBOSE { p_solution("N-2", 2, &sol_nm2_d2); }
 
             if VERBOSE { h(2, "Game N-2+1"); }
             let play = sol_nm2_d2.opt_play.unwrap();
-            let game_nm2p1 = game_n_2.play(play).unwrap();
+            let game_nm2p1 = game_nm2.play(play).unwrap();
             if VERBOSE { pln(&game_nm2p1); }
             let sol_nm2p1_d2 = game_nm2p1.solve_for(2);
             if VERBOSE { p_solution("N-2+1", 2, &sol_nm2p1_d2); }
