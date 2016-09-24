a0d4fa262c66b19c6bb742df79c934c9dfdb340f
David James <davidcjames+xpe@gmail.com>
2016-09-24 00:44:32 -0700

    fix broken tests

--- src/solver/benchmarks.rs
+++ src/solver/benchmarks.rs
@@ -3,7 +3,7 @@ use rand::{SeedableRng, XorShiftRng};
 use random::{random_games};
 use test::Bencher;
 
-fn solve_example_1(k: Count, depth: Count, cache: &mut Cache) {
+fn solve_example_1(k: Count, depth: Count) {
     let seed: [u32; 4] = [1456198685, 762656086, 844876651, 1745969790];
     let mut rng: XorShiftRng = SeedableRng::from_seed(seed);
     let games = random_games(&mut rng);
@@ -12,12 +12,11 @@ fn solve_example_1(k: Count, depth: Count, cache: &mut Cache) {
         games_iter.next_back();
     }
     let game = games_iter.next_back().unwrap();
-    game.solve_for(depth, cache);
+    game.solve(depth);
 }
 
 #[bench]
 fn bench_solve(b: &mut Bencher) {
-    let cache = &mut new_cache(500);
-    solve_example_1(4, 6, cache);
-    b.iter(|| solve_example_1(4, 6, cache))
+    solve_example_1(4, 6);
+    b.iter(|| solve_example_1(4, 6))
 }
