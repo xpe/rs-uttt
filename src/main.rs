d567daf7cffe264d76a9f17eb2b6af2b2b984696
David James <davidcjames+xpe@gmail.com>
2016-09-26 15:59:33 -0700

    Early work on keeping not-deep-enough solutions.

--- src/main.rs
+++ src/main.rs
@@ -130,7 +130,9 @@ fn run_backwards_solve<S: Stack, R: Rng>(trials: u16, stack: &S, rng: &mut R,
 // -- solve function(s) --------------------------------------------------------
 
 fn solve(stack: &Stack, game: &Game, depth: Count) -> Solution {
-    match stack.get(&game, depth) {
+    let stack_get = stack.get(&game, depth);
+    let opt_solution = stack_get.0;
+    match opt_solution {
         Some(solution) => solution,
         None => panic!("Stack '{}' returned no solution", stack.label())
     }
--- src/solver/stack.rs
+++ src/solver/stack.rs
@@ -1,5 +1,5 @@
 use data::{Count, Game};
-use solver::{Layer, Outcome, Solution};
+use solver::{Device, Layer, Outcome, Solution};
 
 /// A solver stack.
 pub trait Stack {
@@ -7,13 +7,23 @@ pub trait Stack {
 
     fn label(&self) -> &str;
 
-    /// Get a solution for the given game and depth from the stack. Starts with
-    /// the highest part of the stack and works its way down. Delegates to
-    /// either 'read' or 'compute' depending on the nature of the device.
+    /// Returns two things:
+    ///
+    /// 1. the first suitable solution for a given game and depth from the
+    /// stack.
+    ///
+    /// 2. Returns a vector of devices (that were tried along the way) that
+    /// had non-suitable solutions.
+    ///
+    /// This function starts with the highest part of the stack and works its
+    /// way down. Delegates to either 'read' or 'compute' depending on the
+    /// nature of the device.
     ///
     /// (Naming note: I chose the name 'get' to convey that it is more general
     /// than 'read' or 'compute'.)
-    fn get(&self, game: &Game, depth: Count) -> Option<Solution> {
+    fn get(&self, game: &Game, depth: Count)
+           -> (Option<Solution>, Vec<Box<Device>>) {
+        let devices: Vec<Box<Device>> = vec![];
         for layer in self.layers().iter() {
             let device = layer.device();
             let opt_solution = if device.supports_read() {
@@ -27,7 +37,7 @@ pub trait Stack {
                 Some(solution) => {
                     if solution.is_deep_enough(depth) {
                         println!("[+] {}", layer.label());
-                        return Some(solution)
+                        return (Some(solution), devices)
                     } else {
                         println!("[-] {}", layer.label());
                         // TODO: Store this layer so that the algorithm can come
@@ -40,7 +50,7 @@ pub trait Stack {
                 },
             }
         }
-        None
+        (None, devices)
     }
 }
 
