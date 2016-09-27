1b319f1be46eea5fe43412e024a8974f15176fba
David James <davidcjames+xpe@gmail.com>
2016-09-26 18:16:48 -0700

    Implement sol_i16. Compiles. Not yet tested.

--- src/solver/db/mod.rs
+++ src/solver/db/mod.rs
@@ -176,11 +176,18 @@ fn game_columns_from(game: &Game) -> (i64, i64, i32) {
     (game_1 as i64, game_2 as i64, game_3 as i32)
 }
 
-// TODO
 /// Converts a Solution to a 16-bit integer.
-#[allow(unused_variables)]
 fn sol_i16(solution: Solution) -> i16 {
-    unimplemented!()
+    let x_outcome: u16 = match solution.outcome {
+        Outcome::Unknown { .. } => 0,
+        Outcome::Tie { .. } => 1,
+        Outcome::Win { player: Player::O, .. } => 2,
+        Outcome::Win { player: Player::X, .. } => 3,
+    };
+    let x_location: u16 = location_u16(solution.opt_play);
+    let x_turns: u16 = solution.outcome.turns() as u16;
+    let x: u16 = x_outcome << 14 | x_location << 7 | x_turns;
+    x as i16
 }
 
 // == helpers for conversions (structs -> database values) =====================
@@ -199,6 +206,18 @@ fn player_u32(opt_player: Option<Player>) -> u32 {
         Some(Player::O) => 0,
         Some(Player::X) => 1,
         None => 2,
+    }
+}
+
+fn location_u16(opt_play: Option<Play>) -> u16 {
+    match opt_play {
+        None => 127_u16,
+        Some(play) => {
+            let location: Loc = play.loc;
+            let row_idx = location.row().as_u8();
+            let col_idx = location.col().as_u8();
+            (row_idx * 9 + col_idx) as u16
+        }
     }
 }
 
@@ -243,10 +262,11 @@ fn solution_from(sol: i16, player: Option<Player>) -> Solution {
 
 // == helper conversions (database values -> structs) ==========================
 
+/// Converts from an 8-bit unsigned integer to an optional location.
 fn opt_loc_from(x: u8) -> Option<Loc> {
+    // TODO: Use math instead of this dreadful pattern match.
     match x {
         127 => None,
-
         0 => Some(Loc::new(RI::R0, CI::C0)),
         1 => Some(Loc::new(RI::R0, CI::C1)),
         2 => Some(Loc::new(RI::R0, CI::C2)),
@@ -256,7 +276,6 @@ fn opt_loc_from(x: u8) -> Option<Loc> {
         6 => Some(Loc::new(RI::R0, CI::C6)),
         7 => Some(Loc::new(RI::R0, CI::C7)),
         8 => Some(Loc::new(RI::R0, CI::C8)),
-
         9 => Some(Loc::new(RI::R1, CI::C0)),
         10 => Some(Loc::new(RI::R1, CI::C1)),
         11 => Some(Loc::new(RI::R1, CI::C2)),
@@ -266,7 +285,6 @@ fn opt_loc_from(x: u8) -> Option<Loc> {
         15 => Some(Loc::new(RI::R1, CI::C6)),
         16 => Some(Loc::new(RI::R1, CI::C7)),
         17 => Some(Loc::new(RI::R1, CI::C8)),
-
         18 => Some(Loc::new(RI::R2, CI::C0)),
         19 => Some(Loc::new(RI::R2, CI::C1)),
         20 => Some(Loc::new(RI::R2, CI::C2)),
@@ -276,7 +294,6 @@ fn opt_loc_from(x: u8) -> Option<Loc> {
         24 => Some(Loc::new(RI::R2, CI::C6)),
         25 => Some(Loc::new(RI::R2, CI::C7)),
         26 => Some(Loc::new(RI::R2, CI::C8)),
-
         27 => Some(Loc::new(RI::R3, CI::C0)),
         28 => Some(Loc::new(RI::R3, CI::C1)),
         29 => Some(Loc::new(RI::R3, CI::C2)),
@@ -286,7 +303,6 @@ fn opt_loc_from(x: u8) -> Option<Loc> {
         33 => Some(Loc::new(RI::R3, CI::C6)),
         34 => Some(Loc::new(RI::R3, CI::C7)),
         35 => Some(Loc::new(RI::R3, CI::C8)),
-
         36 => Some(Loc::new(RI::R4, CI::C0)),
         37 => Some(Loc::new(RI::R4, CI::C1)),
         38 => Some(Loc::new(RI::R4, CI::C2)),
@@ -296,7 +312,6 @@ fn opt_loc_from(x: u8) -> Option<Loc> {
         42 => Some(Loc::new(RI::R4, CI::C6)),
         43 => Some(Loc::new(RI::R4, CI::C7)),
         44 => Some(Loc::new(RI::R4, CI::C8)),
-
         45 => Some(Loc::new(RI::R5, CI::C0)),
         46 => Some(Loc::new(RI::R5, CI::C1)),
         47 => Some(Loc::new(RI::R5, CI::C2)),
@@ -306,7 +321,6 @@ fn opt_loc_from(x: u8) -> Option<Loc> {
         51 => Some(Loc::new(RI::R5, CI::C6)),
         52 => Some(Loc::new(RI::R5, CI::C7)),
         53 => Some(Loc::new(RI::R5, CI::C8)),
-
         54 => Some(Loc::new(RI::R6, CI::C0)),
         55 => Some(Loc::new(RI::R6, CI::C1)),
         56 => Some(Loc::new(RI::R6, CI::C2)),
@@ -316,7 +330,6 @@ fn opt_loc_from(x: u8) -> Option<Loc> {
         60 => Some(Loc::new(RI::R6, CI::C6)),
         61 => Some(Loc::new(RI::R6, CI::C7)),
         62 => Some(Loc::new(RI::R6, CI::C8)),
-
         63 => Some(Loc::new(RI::R7, CI::C0)),
         64 => Some(Loc::new(RI::R7, CI::C1)),
         65 => Some(Loc::new(RI::R7, CI::C2)),
@@ -326,7 +339,6 @@ fn opt_loc_from(x: u8) -> Option<Loc> {
         69 => Some(Loc::new(RI::R7, CI::C6)),
         70 => Some(Loc::new(RI::R7, CI::C7)),
         71 => Some(Loc::new(RI::R7, CI::C8)),
-
         72 => Some(Loc::new(RI::R8, CI::C0)),
         73 => Some(Loc::new(RI::R8, CI::C1)),
         74 => Some(Loc::new(RI::R8, CI::C2)),
@@ -336,7 +348,6 @@ fn opt_loc_from(x: u8) -> Option<Loc> {
         78 => Some(Loc::new(RI::R8, CI::C6)),
         79 => Some(Loc::new(RI::R8, CI::C7)),
         80 => Some(Loc::new(RI::R8, CI::C8)),
-
         _ => panic!("Error 8689"),
     }
 }
